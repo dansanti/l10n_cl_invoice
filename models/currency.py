0eaee1aa3a10e8d533689c7a9e3d167ccd9528bb
Daniel Santibáñez Polanco <dansanti@gmail.com>
2018-02-03 01:29:31 -0300

    [IMP]Multi Moneda
    - Tener cuidado con este update, porque se eliminan campos de currency y bajará los servicios de odoo si no se hace una acualización correcta.
    - Se recomienda, antes de aplicar la actualizacióneliminar los campos currency actuales de en currency_view.xml y currency.py, reiniciar servicios y actualizar, luego de eso aplicar esta actualización

--- models/currency.py
+++ models/currency.py
@@ -2,11 +2,12 @@
 from openerp import fields, models, api
 
 
-class res_currency(models.Model):
+class ResCurrency(models.Model):
     _inherit = "res.currency"
-    _description = "Currency"
 
-    sii_code = fields.Char('SII Code', size=4, readonly=True)
-    sii_desc = fields.Char('SII Description', size=250, readonly=True)
-    sii_dt_from = fields.Date('SII Valid from', readonly=True)
-    
+    code = fields.Char(
+            string="Código",
+        )
+    abreviatura = fields.Char(
+            string="Abreviatura",
+        )
