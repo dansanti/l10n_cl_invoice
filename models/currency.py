4bc535c13a6779d14708da12b75efa1f2769fa13
Daniel Santibáñez Polanco <dansanti@gmail.com>
2018-02-04 08:59:44 -0300

    [REM]Redondeo general que devuelve .0 cuando es o decimal
    Si la moneda tiene configurado 0 Decimal, revuelve entero

--- models/currency.py
+++ models/currency.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from openerp import fields, models, api
+from openerp.tools import float_round
 
+def float_round_custom(value, precision_digits=None, precision_rounding=None, rounding_method='HALF-UP'):
+	result = float_round(value, precision_digits, precision_rounding, rounding_method)
+	if precision_rounding == 1:
+		return int(result)
+	return result
 
 class ResCurrency(models.Model):
     _inherit = "res.currency"
@@ -11,3 +17,19 @@ class ResCurrency(models.Model):
     abreviatura = fields.Char(
             string="Abreviatura",
         )
+
+    @api.v8
+    def round(self, amount):
+        """ Return `amount` rounded according to currency `self`. """
+        return float_round_custom(amount, precision_rounding=self.rounding)
+
+    @api.v7
+    def round(self, cr, uid, currency, amount):
+        """Return ``amount`` rounded  according to ``currency``'s
+           rounding rules.
+
+           :param Record currency: currency for which we are rounding
+           :param float amount: the amount to round
+           :return: rounded float
+        """
+        return float_round_custom(amount, precision_rounding=currency.rounding)
